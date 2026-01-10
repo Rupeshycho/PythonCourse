800309e6fef8e0c4ad76dc0170ee5272a5880a87
Rupeshycho <crrupesh0@gmail.com>
2026-01-10 20:31:51 +0000

    added basics days practice

---
+++ practice4/ArithmeticOperation.py
@@ -0,0 +1,12 @@
+print(f'Remainder of 10 divide by 3 is : {10%3}')
+
+x=10
+x=x+3 #13
+x+=3 #16
+print(x)
+x-=6
+print(x)
+
+y=10+3*4**2
+print(y)
+
---
+++ practice4/whileLoops/While_Loops.py
@@ -0,0 +1,11 @@
+print("---While Loops---")
+
+# while(condtion):
+#     ......
+
+i=1
+while i<=5:
+    print(i*'*')
+    i+=1
+
+print("Outside of the looop: done:")

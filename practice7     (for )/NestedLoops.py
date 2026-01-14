33a5efb9c2532f4123226a49ce77898a8dc3f35b
Rupeshycho <crrupesh0@gmail.com>
2026-01-14 06:34:03 +0000

    Indexing plus 1D and 2D lists added

--- practice7     (for )/NestedLoops.py
+++ practice7     (for )/NestedLoops.py
@@ -24,3 +24,45 @@ for x_count in numbers:
     for count in range(x_count):
         output+='x'
     print(output)    
+
+names=['RupeshAnju','Aarti','Ram','shyam','Alex']
+print(names[0])#RupeshAnju    
+print(names[-1]) #alex
+print(names[2:])  #starts from the 2(Ram) to including last 
+print(names[1:4]) # prints from 1(Aarti) to shyam no inclusion of  4th index 
+print(names[1:]) #now include all items by including  1
+
+print(f'Print all items\n{names[:]}')
+
+print(f'Original list: {names}')
+names[0]='AnjuRupesh'
+print(names)
+
+#WAP to find the largest number in a list
+numbers = [6,28,4,30,10]
+max= numbers[-1]
+for num in numbers:
+    if num> max:
+        max = num
+print(max)      
+matrix=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+#print a matrix rows 
+for row in matrix:
+    print(row)
+
+#print all items of matrix
+for row in matrix:
+    for item in row:
+        print(item)    
+print(row) #print last row 
+
+numbers2=[1,2,3]
+for numb in numbers2:
+    print(numb)    
+
+        

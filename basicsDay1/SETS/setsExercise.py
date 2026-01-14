407f3b9cc3e219e0e622a486da971cbeb5cad7bf
Rupeshycho <crrupesh0@gmail.com>
2026-01-14 09:21:10 +0000

    sets, methods,problems added

---
+++ basicsDay1/SETS/setsExercise.py
@@ -0,0 +1,9 @@
+#Set: A set is an unordered collection of unique items.
+#Duplicate values are not allowed 
+#why sets:
+#store unique items 
+#perform operations like union, intersection 
+#Fast membership test 
+#useful for unique IDs, unique elements 
+#syntax
+# my_set={item1, item2, item3}

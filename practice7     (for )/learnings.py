4c21600cbd83003067e5558e3311740e7b9452da
Rupeshycho <crrupesh0@gmail.com>
2026-01-14 05:40:27 +0000

    for loop with even and odd

---
+++ practice7     (for )/learnings.py
@@ -0,0 +1,28 @@
+for i in range(1,5):
+    print(i*'2')
+
+for item in 'python':
+    print(item)
+    
+    
+for item in ['RupeshAnju', 'John', 'sarah']:
+    print(item)   
+for num in [1,2,3,4]:
+    print(num)
+
+for number in {1,2,3,4,5,5}:
+    print(number)
+
+for num in range(10):
+    print(num)
+
+#print odd number
+print("--Even number--")
+num=(int(input("Enter the last number:")))
+for i in range(2,num+1,2):
+    print(i)
+    
+print("--odd number--")
+for i in range(1,num+1,2):
+    print(i)    
+           

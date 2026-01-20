474db3decd88688f90ab97c65be4483d2586c77d
Rupeshycho <crrupesh0@gmail.com>
2026-01-21 00:40:33 +0545

    try-except-finally added

---
+++ ExceptionHandling/first.py
@@ -0,0 +1,14 @@
+prices=[559,789,"N/A",349]
+print(prices)
+
+for price in prices:
+    print("Vertical List ")
+    print(price)
+        
+try:
+    print(sum(prices))
+    
+except TypeError:
+    print("Check the prices")
+finally: 
+    print("Need help? Contact us ")        

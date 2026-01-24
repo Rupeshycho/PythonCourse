be3de50fea584a69dd6a37aeb73c62eb0e7bcca0
Rupeshycho <crrupesh0@gmail.com>
2026-01-25 00:17:21 +0545

    General concepts added

--- PMosh/tuples.py
+++ PMosh/tuples.py
@@ -1,4 +1,22 @@
 #Immutable (Tuples)
 numbers=(1,2,3,4,4)
-print(numbers[4])
-print(numbers.count(4))
+# numbers[0]=10 #cannot change 
+print("index of 4: ",numbers[4])
+
+print("Repeatition of 4: ",numbers.count(4))
+
+
+coordinates=(1,2,3)
+
+print("coordinates[1]*[2]*[3]=",coordinates[0]*coordinates[1]*coordinates[2])
+
+x,y,z =coordinates
+
+print("x,y,z =coordinates")
+print(x)
+print(y)
+print(z)
+
+print("x*y*z= ",x*y*z)
+
+

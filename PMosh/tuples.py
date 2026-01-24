45f247700fe726d959f38eda4840f9aa583a0f43
Rupeshycho <crrupesh0@gmail.com>
2026-01-25 00:24:51 +0545

    unpacking( x,y,z =coordinates) added

--- PMosh/tuples.py
+++ PMosh/tuples.py
@@ -47,6 +47,8 @@ lst.append(99)
 numbers = tuple(lst)
 print("Converted back to tuple:", numbers)
 
+
+#Covering All
 numbers = (1, 2, 3, 4, 4)
 
 print("Tuple:", numbers)
@@ -68,9 +70,23 @@ for n in numbers:
     print(n, end=" ")
 print()
 
+numbers = (1, 2, 3, 4, 4)
 print("Loop with index:")
 for i, n in enumerate(numbers):
     print(i, n)
 
 #Unpacking 
 print("Unpacking ".center(40,"="))
+
+coordinates=(1,2,3)
+
+print("coordinates[1]*[2]*[3]=",coordinates[0]*coordinates[1]*coordinates[2])
+
+x,y,z =coordinates
+
+print("x,y,z =coordinates")
+print(x)
+print(y)
+print(z)
+
+print("x*y*z= ",x*y*z)

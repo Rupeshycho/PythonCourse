0a5ed023a6f6f99ce1abd89bb4971e50ab072e18
Rupeshycho <crrupesh0@gmail.com>
2026-01-25 22:41:56 +0545

    functions added

---
+++ PMosh/functions.py
@@ -0,0 +1,11 @@
+def greet_user():
+    
+    name=input("What's your name?\n")
+    
+    print(f'Hey there, {name}.')
+    age=input("What's your age?\n")
+    print(f'so you are {int(age)} years old.')
+    return name
+print(">>>Interview>>>")    
+name=greet_user()   
+print(f'Thank you Mr.{name}.')

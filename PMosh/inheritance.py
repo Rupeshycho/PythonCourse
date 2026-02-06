1be2d67af85f28da444bbf76d7ecb8b4e134aa95
Rupeshycho <crrupesh0@gmail.com>
2026-02-06 09:58:24 +0545

    Inheritance added

---
+++ PMosh/inheritance.py
@@ -0,0 +1,30 @@
+# class Dog: 
+#     def walk(self):
+#         print("Walking Dog ")
+        
+
+# class Cat:
+#     def walk(self):
+#         print("Walk")        
+
+
+class Mammal:
+    def walk(self):
+        print("Walk")
+        
+class Dog(Mammal):
+    def bark(self):
+        print("Dog is barking ")
+          
+class Cat(Mammal):
+    def be_annoying(self):
+        print("Cat is annoying compared to dogs ✨🤣")        
+
+Dog1=Dog()
+Dog1.walk()
+Dog1.bark()
+
+cat1=Cat()
+cat1.walk()
+cat1.be_annoying()
+        

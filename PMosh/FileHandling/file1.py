447c039b312096b3802a40c0cc094c40660f0569
Rupeshycho <crrupesh0@gmail.com>
2026-02-18 23:46:05 +0545

    r,w added

--- PMosh/FileHandling/file1.py
+++
@@ -1,7 +0,0 @@
-with open('PMosh\FileHandling\welcome.txt', 'r+') as f: #for read and write use r+ mode
-    print(f.read())
-    print(f.write('Welcome to Python Programming!'))  # This will raise an error because the file is opened in read mode
-    
-f=open('PMosh/FileHandling/welcome.txt','r+')
-print(f.read())    
-    
---
+++ PMosh/FileHandling/write/w.py
@@ -0,0 +1,9 @@
+f = open("PMosh/FileHandling/write/sample.txt", "a")
+f.write("Truncated previous texts\n")
+f.close()
+
+# a= append (add at the end )
+# f= open("pythoncourse\PMosh\FileHandling\write\sample.txt","a")
+# f=open("PMosh\FileHandling\write\sample.txt","a")
+# f.write("Then , I will move to react. js ")
+# f.close()

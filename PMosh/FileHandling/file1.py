27015b01dd6d1c6160689eb644bcbf460567622f
Rupeshycho <crrupesh0@gmail.com>
2026-02-18 19:43:08 +0545

    initial commit

--- PMosh/FileHandling/file1.py
+++ PMosh/FileHandling/file1.py
@@ -1,6 +1,6 @@
-with open('demofile.txt', 'r') as f:
+with open('PMosh\FileHandling\welcome.txt', 'r+') as f: #for read and write use r+ mode
     print(f.read())
-    print(f.write)
+    print(f.write('Welcome to Python Programming!'))  # This will raise an error because the file is opened in read mode
     
 f=open('PMosh/FileHandling/welcome.txt','r+')
 print(f.read())    
